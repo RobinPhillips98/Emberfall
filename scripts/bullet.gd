e2eed233646b4b47ff874ea09ec8ada10db24cc2
Robin Phillips <robinphillips462@gmail.com>
2025-01-31 18:57:43 -0500

    Attack functionality implemented.

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 var travelled_distance = 0
+@onready var player = get_node("/root/Game/Player")
+@onready var bow = player.bow
 
 func _physics_process(delta):
 	const SPEED = 1000
-	const RANGE = 1200
+	var RANGE = bow.range
 	
 	var direction = Vector2.RIGHT.rotated(rotation)
 	position += direction * SPEED * delta
@@ -18,4 +20,4 @@ func _on_body_entered(body):
 	queue_free()
 	
 	if body.has_method("take_damage"):
-			body.take_damage(1)
+			body.take_damage(bow.damage)
