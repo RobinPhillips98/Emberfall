a357162bed0a5769502167e976985a136a4991f3
Robin Phillips <robinphillips462@gmail.com>
2025-02-03 19:47:10 -0500

    Implemented skeleton attacks. Polished some other files.

---
+++ scripts/skeleton-bow.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@onready var player = get_node("/root/Game/Player")
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	look_at(player.global_position)
+
+func shoot():
+	const ARROW = preload("res://scenes/arrow.tscn")
+	var new_arrow = ARROW.instantiate()
+	new_arrow.global_position = %ShootingPoint.global_position
+	new_arrow.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_arrow)
