fd44ebf3c979b4872c13ae3dfda37da41df0aa1f
Robin Phillips <robinphillips462@gmail.com>
2025-02-05 14:12:36 -0500

    Minor bug fixes

--- scripts/equipment/bow.gd
+++ scripts/equipment/bow.gd
@@ -1,22 +1,20 @@
 extends Area2D
 
-var travelled_distance = 0
-@onready var player = get_node("/root/Game/Player")
-
 func _physics_process(delta):
-	const SPEED = 1000
-	var RANGE = 1200
+	look_at(get_global_mouse_position())
 	
-	var direction = Vector2.RIGHT.rotated(rotation)
-	position += direction * SPEED * delta
-
-	travelled_distance += SPEED * delta
-	if travelled_distance > RANGE:
-		queue_free()
-
-
-func _on_body_entered(body):
-	queue_free()
+	if Input.is_action_just_pressed("ranged_attack"):
+		shoot()
 	
-	if body.has_method("take_damage"):
-			body.take_damage(10)
+	#var enemies_in_range = get_overlapping_bodies()
+	#
+	#if enemies_in_range.size() > 0:
+		#var target_enemy = enemies_in_range[0]
+		#look_at(target_enemy.global_position)
+
+func shoot():
+	const ARROW = preload("res://scenes/equipment/arrow.tscn")
+	var new_arrow = ARROW.instantiate()
+	new_arrow.global_position = %ShootingPoint.global_position
+	new_arrow.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_arrow)
