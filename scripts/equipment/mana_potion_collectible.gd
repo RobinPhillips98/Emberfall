aee6325719ec9699fe934ecbe6e5236490b8c4b1
Robin Phillips <robinphillips462@gmail.com>
2025-02-05 12:08:13 -0500

    Removed inventory system. Completed file system changes.

--- scripts/equipment/mana_potion_collectible.gd
+++ scripts/equipment/mana_potion_collectible.gd
@@ -1,11 +1,7 @@
 extends StaticBody2D
 
-@export var item: InvItem
-var player = null
-
 func _on_interactable_area_body_entered(body):
-	if body.has_method("collect"):
-		player = body
-		player.collect(item)
+	if body.has_method("gain_potion"):
+		body.gain_potion("mana_potion")
 		await get_tree().create_timer(0.1).timeout
 		queue_free()
