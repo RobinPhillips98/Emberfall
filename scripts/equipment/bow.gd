0afa5fbf19b5edfd227b60cd76cb9e1cd44c6512
Robin Phillips <robinphillips462@gmail.com>
2025-02-12 11:10:52 -0500

    Implemented fireball.

--- scripts/equipment/bow.gd
+++ scripts/equipment/bow.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var player = get_node("/root/Game/Player")
 const STAMINA_COST = 50
+const MANA_COST = 25
 
 func _physics_process(delta):
 	input()
@@ -18,6 +19,12 @@ func input():
 	
 	if Input.is_action_just_pressed("ranged_attack") and player.get_stamina() >= STAMINA_COST:
 		shoot()
+		#for i in range(PlayerVariables.level):
+			#shoot()
+			#await get_tree().create_timer(0.1).timeout
+		
+	if Input.is_action_just_pressed("fireball") and player.get_mana() >= MANA_COST:
+		fireball()
 
 func shoot():
 	player.modify_stamina(-STAMINA_COST)
@@ -26,3 +33,11 @@ func shoot():
 	new_arrow.global_position = %ShootingPoint.global_position
 	new_arrow.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_arrow)
+
+func fireball():
+	player.modify_mana(-MANA_COST)
+	const FIREBALL = preload("res://scenes/equipment/fireball.tscn")
+	var new_fireball = FIREBALL.instantiate()
+	new_fireball.global_position = %ShootingPoint.global_position
+	new_fireball.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_fireball)
